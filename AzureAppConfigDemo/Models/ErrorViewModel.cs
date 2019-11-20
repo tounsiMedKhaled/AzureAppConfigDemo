322fe1636afa243e94913fb86f4839e492a191fe
Mohamed Khaled Tounsi <tounsi.medkhaled@hotmail.fr>
2019-11-19 21:23:17 -0500

    Add project files.

---
+++ AzureAppConfigDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureAppConfigDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ AzureAppConfigDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+
+namespace AzureAppConfigDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+             .ConfigureAppConfiguration((hostingContext, config) =>
+             {
+                 var settings = config.Build();
+                 config.AddAzureAppConfiguration(options =>
+                 {
+                     options.Connect(settings["ConnectionStrings:AppConfig"])
+                         .UseFeatureFlags();
+                 });
+             })
+                .UseStartup<Startup>();
+    }
+}
